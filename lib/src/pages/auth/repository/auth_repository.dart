f67f76f277885e647d366ba812acd6b4b84e3536
lucasanbe <lucas.andradebez@gmail.com>
2023-08-21 11:50:07 -0300

    criando método de validação via token, salvamento do token local e melhorando o método de autenticação

--- lib/src/pages/auth/repository/auth_repository.dart
+++ lib/src/pages/auth/repository/auth_repository.dart
@@ -1,28 +1,46 @@
 import 'package:greengrocer/src/constants/endpoints.dart';
 import 'package:greengrocer/src/models/user_model.dart';
+import 'package:greengrocer/src/pages/auth/result/auth_result.dart';
 import 'package:greengrocer/src/services/http_manager.dart';
+
+import 'auth_errors.dart' as authErrors;
 
 class AuthRepository {
   final HttpManager _httpManager = HttpManager();
 
-  Future signIn({required String email, required String password}) async {
+  Future<AuthResult> validateToken(String token) async {
     final result = await _httpManager.restRequest(
-      url: Endpoints.signin,
+      url: Endpoints.validateToken,
       method: HttpMethods.post,
-      body: {
-        "email": email,
-        "password": password,
+      headers: {
+        'X-Parse-Session-Token': token,
       },
     );
 
     if (result['result'] != null) {
-      print('SignIn funcionou!');
-      final user = UserModel.fromJson(
-        result['result'],
-      );
-      print(user);
+      final user = UserModel.fromJson(result['result']);
+      return AuthResult.success(user);
     } else {
-      print('SignIn não funcionou');
+      return AuthResult.error(authErrors.authErrorsString(result['error']));
+    }
+  }
+
+  Future<AuthResult> signIn(
+      {required String email, required String password}) async {
+    final result = await _httpManager.restRequest(
+      url: Endpoints.signin,
+      method: HttpMethods.post,
+      body: {
+        'email': email,
+        'password': password,
+      },
+    );
+
+    if (result['result'] != null) {
+      final user = UserModel.fromJson(result['result']);
+      return AuthResult.success(user);
+    } else {
+      return AuthResult.error(authErrors.authErrorsString(result['error']));
     }
   }
 }
