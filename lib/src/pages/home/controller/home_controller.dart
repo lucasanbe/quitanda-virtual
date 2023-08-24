58014a062668396482ad6eb27025b2f03456cd95
lucasanbe <lucas.andradebez@gmail.com>
2023-08-24 15:56:33 -0300

    tornando funcional a tela de exibição dos produtos, consumindo da API, otimizando carregamento das imagens e informações dos produtos

--- lib/src/pages/home/controller/home_controller.dart
+++ lib/src/pages/home/controller/home_controller.dart
@@ -1,27 +1,44 @@
 import 'package:get/get.dart';
 import 'package:greengrocer/src/models/category_model.dart';
+import 'package:greengrocer/src/models/item_model.dart';
 import 'package:greengrocer/src/pages/home/result/home_result.dart';
 import 'package:greengrocer/src/services/utils_services.dart';
 
 import '../repository/home_repository.dart';
 
+const int itemsPerPage = 6;
+
 class HomeController extends GetxController {
   final homeRepository = HomeRepository();
   final utilsServices = UtilsServices();
 
-  bool isLoading = false;
+  bool isCategoryLoading = false;
+  bool isProductLoading = true;
   List<CategoryModel> allCategories = [];
+  CategoryModel? currentCategory;
+  List<ItemModel> get allProducts => currentCategory?.items ?? [];
 
-  void setLoading(bool value) {
-    isLoading = value;
+  void setLoading(bool value, {bool isProduct = false}) {
+    if (!isProduct) {
+      isCategoryLoading = value;
+    } else {
+      isProductLoading = value;
+    }
 
     update();
   }
 
   @override
-  onInit() {
+  void onInit() {
     super.onInit();
     getAllCategories();
+  }
+
+  void selectCategory(CategoryModel category) {
+    currentCategory = category;
+    update();
+    if (currentCategory!.items.isNotEmpty) return;
+    getAllProducts();
   }
 
   Future<void> getAllCategories() async {
@@ -32,6 +49,10 @@ class HomeController extends GetxController {
     homeResult.when(
       success: (data) {
         allCategories.assignAll(data);
+
+        if (allCategories.isEmpty) return;
+
+        selectCategory(allCategories.first);
       },
       error: (message) {
         utilsServices.showToast(
@@ -40,5 +61,24 @@ class HomeController extends GetxController {
         );
       },
     );
+  }
+
+  Future<void> getAllProducts() async {
+    setLoading(true, isProduct: true);
+    Map<String, dynamic> body = {
+      'page': currentCategory!.pagination,
+      'categoryId': currentCategory!.id,
+      'itemsPerPage': itemsPerPage,
+    };
+    HomeResult<ItemModel> result = await homeRepository.getAllProducts(body);
+    setLoading(false, isProduct: true);
+    result.when(success: (data) {
+      currentCategory!.items = data;
+    }, error: (message) {
+      utilsServices.showToast(
+        message: message,
+        isError: true,
+      );
+    });
   }
 }
--- lib/src/pages/home/repository/home_repository.dart
+++ lib/src/pages/home/repository/home_repository.dart
@@ -1,5 +1,6 @@
 import 'package:greengrocer/src/constants/endpoints.dart';
 import 'package:greengrocer/src/models/category_model.dart';
+import 'package:greengrocer/src/models/item_model.dart';
 import 'package:greengrocer/src/pages/home/result/home_result.dart';
 
 import '../../../services/http_manager.dart';
@@ -19,7 +20,29 @@ class HomeRepository {
       return HomeResult<CategoryModel>.success(data);
     } else {
       return HomeResult.error(
-          'Ocorreu um erro inesperado ao recuperar as categorias');
+        'Ocorreu um erro inesperado ao recuperar as categorias',
+      );
+    }
+  }
+
+  Future<HomeResult<ItemModel>> getAllProducts(
+      Map<String, dynamic> body) async {
+    final result = await _httpManager.restRequest(
+      url: Endpoints.getAllProducts,
+      method: HttpMethods.post,
+      body: body,
+    );
+
+    if (result['result'] != null) {
+      List<ItemModel> data = List<Map<String, dynamic>>.from(result['result'])
+          .map(ItemModel.fromJson)
+          .toList();
+
+      return HomeResult<ItemModel>.success(data);
+    } else {
+      return HomeResult.error(
+        'Ocorreu um erro inesperado ao recuperar os itens',
+      );
     }
   }
 }
